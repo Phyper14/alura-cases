d58a23f6f6e1541c5950670974a71236b41a9296
Phyper14 <martins14.pablo@gmail.com>
2023-04-12 18:15:47 -0300

    add global styles

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Link from '../src/components/Link';
 
+const Title = ({children, as}) => {
+    const Tag = as;
+    return (
+        <>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{
+                `${Tag} {
+                color: red;
+            }`
+            }</style>
+        </>
+    )
+}
 
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home</h1>
+            <Title as={"h1"} >Alura Cases - Home</Title>
             <Link href="/faq">
                 Ir para a página do FAQ
             </Link>
